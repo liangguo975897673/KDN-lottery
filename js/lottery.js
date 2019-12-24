5a4924105a4d6a4a895d8226e22c4716a6d34c65
liangguo <975897673@qq.com>
2019-12-24 16:20:05 +0800

    提交项目

---
+++ js/lottery.js
@@ -0,0 +1,33 @@
+var prize = [
+    {
+        "name": "活动结束",
+        "prizeCount": 0,
+        "prizeNum": 0,
+        "sort": 5
+    },
+  {
+    "name": "特等奖",
+    "prizeCount": 1,
+    "prizeNum": 1,
+    "sort": 4
+  },
+   {
+    "name": "一等奖",
+    "prizeCount": 10,
+    "prizeNum": 10,
+    "sort": 3
+  },
+   {
+    "name": "二等奖",
+    "prizeCount": 20,
+    "prizeNum": 10,
+    "sort": 2
+  },
+   {
+    "name": "三等奖",
+    "prizeCount": 30,
+    "prizeNum": 10,
+    "sort": 1
+  }
+
+]
